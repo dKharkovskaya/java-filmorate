8f50493ccc9c35fe593622f870151102d9d58194
Daria Kharkovskaya <dicsi59@gmail.com>
2025-01-14 21:18:57 +0300

    Спринт 10 исправления

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -3,7 +3,9 @@ package ru.yandex.practicum.filmorate.controller;
 import org.springframework.web.bind.annotation.*;
 import lombok.extern.slf4j.Slf4j;
 import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.LocalDate;
 import java.util.Collection;
@@ -59,12 +61,30 @@ public class FilmController {
     @PutMapping
     public Film update(@RequestBody Film film) {
         log.info("PUT /film/{}", film.getName());
-        if (films.containsKey(film.getId())) {
-            films.get(film.getId()).setDescription(film.getDescription());
-        //    films.get(film.getId()).setName(film.getName());
-        //    films.get(film.getId()).setDuration(film.getDuration());
-        //    films.get(film.getId()).setReleaseDate(film.getReleaseDate());
+        if (film.getId() == null) {
+            throw new ConditionsNotMetException("Id должен быть указан");
         }
-        return film;
+        if (films.containsKey(film.getId())) {
+            Film oldFilm = films.get(film.getId());
+            if (film.getName() == null || film.getName().isBlank()) {
+                throw new ConditionsNotMetException("Название фильма не может быть пустым");
+            }
+            if (film.getDescription().length() > 200) {
+                throw new ConditionsNotMetException("Длина описания превышает 200 символов");
+            }
+            if (film.getDuration() <= 0) {
+                throw new ConditionsNotMetException("Продолжительность фильма должна быть положительным числом");
+            }
+            if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+                throw new ConditionsNotMetException("дата релиза — не раньше 28 декабря 1895 года;");
+            }
+            // если фильм найден и все условия соблюдены, обновляем её содержимое
+            oldFilm.setName(film.getName());
+            oldFilm.setDuration(film.getDuration());
+            oldFilm.setReleaseDate(film.getReleaseDate());
+            oldFilm.setDescription(film.getDescription());
+            return oldFilm;
+        }
+        throw new NotFoundException("Пост с id = " + film.getId() + " не найден");
     }
 }
