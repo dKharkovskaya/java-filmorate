7bc7db99808e5cb97e3c472bbbefe8aa70db3dec
Daria Kharkovskaya <dicsi59@gmail.com>
2025-02-22 18:46:53 +0300

    Сплит 12 изменения по стилю

--- src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
@@ -1,10 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
-import lombok.NonNull;
 
 @Data
 public class Mpa {
